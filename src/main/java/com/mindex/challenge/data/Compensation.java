07256917c2277b72328ef73ac5931ea275269f84
Nikhil Raina <nikhilrainathegreat@gmail.com>
2021-03-23 16:25:33 -0400

    Updated the effectiveDate to allow UTC Structure

--- src/main/java/com/mindex/challenge/data/Compensation.java
+++ src/main/java/com/mindex/challenge/data/Compensation.java
@@ -1,22 +1,26 @@
 package com.mindex.challenge.data;
 
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Objects;
 
 public class Compensation {
     private Employee employee;
     private int salary;
-    // Not sure what type of dates needs to be implemented here
-    private Date effectiveDate;
 
-    public Compensation(Employee employee, int salary, Date effectiveDate) {
+    // Not sure what type of dates needs to be implemented here
+    // storing UTC time
+    private LocalDateTime effectiveDate;
+
+    public Compensation(Employee employee, int salary, LocalDateTime effectiveDate) {
         this.employee = employee;
         this.salary = salary;
         this.effectiveDate = effectiveDate;
     }
 
     public Employee getEmployee() {
-        return employee;
+        return this.employee;
     }
 
     public void setEmployee(Employee employee) {
@@ -24,19 +28,21 @@ public class Compensation {
     }
 
     public int getSalary() {
-        return salary;
+        return this.salary;
     }
 
     public void setSalary(int salary) {
         this.salary = salary;
     }
 
-    public Date getEffectiveDate() {
-        return effectiveDate;
+    public LocalDateTime getEffectiveDate() {
+        return this.effectiveDate;
     }
 
-    public void setEffectiveDate(Date effectiveDate) {
-        this.effectiveDate = effectiveDate;
+    public void setEffectiveDate(LocalDateTime effectiveDate) {
+        // Default is EST for data consistency
+        ZonedDateTime thisZone = effectiveDate.atZone(ZoneId.of("America/New_York")).withZoneSameInstant(ZoneId.of("UTC"));
+        this.effectiveDate = thisZone.toLocalDateTime();
     }
 
     @Override
@@ -52,9 +58,9 @@ public class Compensation {
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Compensation comp = (Compensation) o;
-        return getSalary() == comp.salary &&
-                getEmployee().equals(comp.employee) &&
-                getEffectiveDate().equals(comp.effectiveDate);
+        return getSalary() == comp.getSalary() &&
+                getEmployee().equals(comp.getEmployee()) &&
+                getEffectiveDate().equals(comp.getEffectiveDate());
     }
 
     @Override
