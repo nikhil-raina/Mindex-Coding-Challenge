569e3b1eda683b35230463450029e6f95212e2ad
Nikhil Raina <nikhilrainathegreat@gmail.com>
2021-03-23 02:45:55 -0400

    Using EmployeeController, created the CompensationController class

---
+++ src/main/java/com/mindex/challenge/controller/CompensationController.java
@@ -0,0 +1,31 @@
+package com.mindex.challenge.controller;
+
+import com.mindex.challenge.data.Compensation;
+import com.mindex.challenge.service.CompensationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestController
+public class CompensationController {
+    private static final Logger LOG = LoggerFactory.getLogger(CompensationController.class);
+
+    @Autowired
+    private CompensationService compensationService;
+
+    @PostMapping("/employee/compensation")
+    public Compensation create(@RequestBody Compensation compensation) {
+        LOG.debug("Received compensation create request for [{}]", compensation);
+
+        return compensationService.create(compensation);
+    }
+
+    @GetMapping("/employee/compensation/{id}")
+    public Compensation read(@PathVariable String id) {
+        LOG.debug("Received compensation read request for employeeId [{}]", id);
+
+        return compensationService.read(id);
+    }
+
+}
