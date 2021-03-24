0e8ad4a35110c9cf67075d277759d05245826d10
Nikhil Raina <nikhilrainathegreat@gmail.com>
2021-03-23 22:30:34 -0400

    Compensation (#2)
    * Created the compensation class
    
    * Created the CompensationService interface
    
    * Using EmployeeController, created the CompensationController class
    
    * Created CompensationRepository to manage all the inserts of compensations
    
    * Updated the query creation by sending for the employee object due to the Compensation class structure
    
    * Updated the effectiveDate to allow UTC Structure
    
    * Implemented the read and create in CompensationServiceImpl. Need to test it out.
    
    * created a custom_database_compensation for testing
    
    * Updated to preload compensation data into the CompensationRepository
    
    * Deserializing mapper to accept the date for testing purpose
    
    * Added more data
    
    * Updated Compensation to be able to create compensations
    
    * Added logs for information that the correct objs are being returned
    
    * Updated the date format
    
    * Updated to add string input to make it easier to input data
    
    * Completed the CompensationTest with 90% coverage.
    
    * Refactored emp1 to emp2
    
    * Added tests for CompensationServiceImplTest. 100% coverage
    
    * Updated the database to match the employee_database.json data
    
    * Reverted the original database
    
    * Added missing data to make the database consistent
    
    * Shows the comp data for verification
    
    * Formulated the toString for Compensation

--- src/main/java/com/mindex/challenge/DataBootstrap.java
+++ src/main/java/com/mindex/challenge/DataBootstrap.java
@@ -1,7 +1,10 @@
 package com.mindex.challenge;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mindex.challenge.dao.CompensationRepository;
 import com.mindex.challenge.dao.EmployeeRepository;
+import com.mindex.challenge.data.Compensation;
 import com.mindex.challenge.data.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -14,24 +17,37 @@ import java.io.InputStream;
 public class DataBootstrap {
     private static final String DATASTORE_LOCATION = "/static/employee_database.json";
     private static final String DATASTORE_LOCATION_CUSTOM = "/static/custom_database.json";
+    private static final String DATASTORE_LOCATION_CUSTOM_COMPENSATION = "/static/custom_database_compensation.json";
 
     @Autowired
     private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private CompensationRepository compensationRepository;
+
 
     @Autowired
     private ObjectMapper objectMapper;
 
     @PostConstruct
     public void init() {
-//        InputStream inputStream = this.getClass().getResourceAsStream(DATASTORE_LOCATION);
-        InputStream inputStream = this.getClass().getResourceAsStream(DATASTORE_LOCATION_CUSTOM);
+        InputStream inputStream = this.getClass().getResourceAsStream(DATASTORE_LOCATION);
+//        InputStream inputStream = this.getClass().getResourceAsStream(DATASTORE_LOCATION_CUSTOM);
+        InputStream inputStreamCompensation = this.getClass().getResourceAsStream(DATASTORE_LOCATION_CUSTOM_COMPENSATION);
 
         Employee[] employees = null;
+        Compensation[] compensations = null;
 
         try {
             employees = objectMapper.readValue(inputStream, Employee[].class);
+            this.objectMapper.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
+            compensations = this.objectMapper.readValue(inputStreamCompensation, Compensation[].class);
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+
+        for (Compensation compensation: compensations) {
+            compensationRepository.insert(compensation);
         }
 
         for (Employee employee : employees) {
---
+++ src/main/java/com/mindex/challenge/data/Compensation.java
@@ -0,0 +1,71 @@
+package com.mindex.challenge.data;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+public class Compensation {
+
+    @JsonSerialize
+    private Employee employee;
+    private int salary;
+
+    // Not sure what type of dates needs to be implemented here
+    // storing UTC time
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime effectiveDate;
+
+    public Compensation(){ }
+
+    public Employee getEmployee() {
+        return this.employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public int getSalary() {
+        return this.salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public LocalDateTime getEffectiveDate() {
+        return this.effectiveDate;
+    }
+
+    public void setEffectiveDate(String effectiveDate) {
+        // Default is EST for data consistency
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        this.effectiveDate = LocalDateTime.parse(effectiveDate, formatter);
+    }
+
+    @Override
+    public String toString() {
+        return "Compensation{" +
+                "employee=" + getEmployee().toString() +
+                ", salary=" + getSalary() +
+                ", effectiveDate=" + getEffectiveDate() +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Compensation comp = (Compensation) o;
+        return getSalary() == comp.getSalary() &&
+                getEmployee().equals(comp.getEmployee()) &&
+                getEffectiveDate().equals(comp.getEffectiveDate());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getEmployee(), getSalary(), getEffectiveDate());
+    }
+}
